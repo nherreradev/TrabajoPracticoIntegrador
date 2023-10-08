78cc98e347b935c915cec44fce5c572063b2fa49
atomasini <tomasiniarnaldo@gmail.com>
2023-10-08 02:51:07 -0300

    Agregado Test Unitario para testinversor y definicion de perfil inversor y arreglos

---
+++ tpi/src/test/java/com/unlam/tpi/RespuestaServicioTest.java
@@ -0,0 +1,42 @@
+package com.unlam.tpi;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.unlam.tpi.dto.CategoriaDTO;
+import com.unlam.tpi.dto.RespuestaDTO;
+import com.unlam.tpi.servicio.CategoriaServicio;
+import com.unlam.tpi.servicio.RespuestaServicio;
+
+@SpringBootTest
+public class RespuestaServicioTest {
+
+	@Autowired
+	private RespuestaServicio  respuestaServicio;
+
+	@Test
+	public void testQuePuedaGuardarCategoria() {
+		RespuestaDTO respuesta = new RespuestaDTO();
+		respuesta.setNombre("Respuesta A");
+		respuesta.setValor(10);
+		respuesta.setOrden(1);
+		getRespuestaServicio().guardar(respuesta);
+	}
+	
+	@Test
+	public void testQuePuedaListarCategoria() {
+		assertTrue(getRespuestaServicio().listar().size()>0);
+	}
+
+	public RespuestaServicio getRespuestaServicio() {
+		return respuestaServicio;
+	}
+
+	public void setRespuestaServicio(RespuestaServicio respuestaServicio) {
+		this.respuestaServicio = respuestaServicio;
+	}
+	
+}
