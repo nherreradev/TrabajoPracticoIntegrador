8d3a8038cec25162cca1762085cd5ad41b32b8f6
nherreradev <nherrera3276@gmail.com>
2023-10-20 22:51:08 -0300

    TPI-Cartera | Avances

--- tpi/src/main/java/com/unlam/tpi/controlador/PanelesControladorImpl.java
+++ tpi/src/main/java/com/unlam/tpi/controlador/PanelesControladorImpl.java
@@ -1,7 +1,12 @@
 package com.unlam.tpi.controlador;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 import com.unlam.tpi.interfaces.PanelesServicio;
+import com.unlam.tpi.interfaces.PlaneadorDeTareasServicio;
 import com.unlam.tpi.modelo.persistente.Instrumento;
 
 @RestController
@@ -20,11 +26,20 @@ public class PanelesControladorImpl implements PanelesControlador {
 	@Autowired
 	PanelesServicio panelesService;
 
+	@Autowired
+	PlaneadorDeTareasServicio planeadorDeTareas;
+
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+	@PostConstruct
+	public void iniciarTareaProgramada() {
+		 //scheduler.scheduleAtFixedRate(this::getPanelDeAcciones, 0, 2, TimeUnit.SECONDS);
+	}
+
 	@Override
 	@GetMapping("/acciones")
 	public ResponseEntity<String> getPanelDeAcciones() {
-		List<Instrumento> panelAcciones = panelesService.getPanelDeAcciones().values().stream()
-				.collect(Collectors.toList());
+		List<Instrumento> panelAcciones = panelesService.getPanelDeAcciones().values().stream().collect(Collectors.toList());
 		String json = new Gson().toJson(panelAcciones);
 		return ResponseEntity.ok(json);
 	}
@@ -32,12 +47,9 @@ public class PanelesControladorImpl implements PanelesControlador {
 	@Override
 	@GetMapping("/bonos")
 	public ResponseEntity<String> getPanelDeBonos() {
-		List<Instrumento> panelBonos = panelesService.getPanelDeBonos().values().stream()
-			    .collect(Collectors.toList());
-
-			String json = new Gson().toJson(panelBonos);
-
-			return ResponseEntity.ok(json);
+		List<Instrumento> panelBonos = panelesService.getPanelDeBonos().values().stream().collect(Collectors.toList());
+		String json = new Gson().toJson(panelBonos);
+		return ResponseEntity.ok(json);
 
 	}
 
