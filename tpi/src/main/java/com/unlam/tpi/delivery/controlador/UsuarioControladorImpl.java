860c0668cbaed269d6534c5433199c4d558b84be
Machi <josemiguelmc_1998@hotmail.com>
2023-10-28 14:37:13 -0300

    test deploy

--- tpi/src/main/java/com/unlam/tpi/delivery/controlador/UsuarioControladorImpl.java
+++ tpi/src/main/java/com/unlam/tpi/delivery/controlador/UsuarioControladorImpl.java
@@ -19,7 +19,7 @@ public class UsuarioControladorImpl implements UsuarioControlador {
 	@Override
 	@GetMapping("/index")
 	public String bienvenido() {
-		return "¡Bienvenido al índice de la API! 28/10 14:14";
+		return "¡Bienvenido al índice de la API! 28/10 14:36 test deploy ....andando";
 	}
 
 	@Override
